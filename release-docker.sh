5e5956a9bc60e31aa7a625433947cd48eeb1940f
dssysolyatin <dmsysolyatin@gmail.com>
2024-07-07 23:19:47 +0300

    Generate pre-release within release.yaml instead of release.

--- release-docker.sh
+++ release-docker.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
 ARCHITECTURE=$(uname -m)
-if [ "$ARCHITECTURE" == "aarch64" ] || [ "$ARCHITECTURE" == "arm64" ]; then
 
-else
-    echo "The architecture is not arm64. Current architecture: $ARCHITECTURE. Please run script on arm64 machine"
-    exit
+# Check if the architecture is arm64
+if [ "$ARCHITECTURE" != "aarch64" ] && [ "$ARCHITECTURE" != "arm64" ]; then
+    echo "The architecture is not arm64. Current architecture: $ARCHITECTURE. Please run the script on an arm64 machine."
+    exit 1
 fi
 
 VERSION=v$(cargo pkgid | cut -d "#" -f2)
 
-docker build -t dssysolyatin/telegram2photoprism:${VERSION}-arm64 .
+docker build -t dssysolyatin/telegram2photoprism:${VERSION}-arm64 --push .
 
-docker manifest create dssysolyatin/telegram2photoprism:${VERSION} \
-dssysolyatin/telegram2photoprism:${VERSION}-arm64 \
-dssysolyatin/telegram2photoprism:${VERSION}-amd64
-
+docker manifest create dssysolyatin/telegram2photoprism:${VERSION} dssysolyatin/telegram2photoprism:${VERSION}-arm64 dssysolyatin/telegram2photoprism:${VERSION}-amd64
 docker manifest push dssysolyatin/telegram2photoprism:${VERSION}
 
-docker manifest create dssysolyatin/telegram2photoprism:latest \
-dssysolyatin/telegram2photoprism:${VERSION}-arm64 \
-dssysolyatin/telegram2photoprism:${VERSION}-amd64
-
+docker manifest rm dssysolyatin/telegram2photoprism:latest
+docker manifest create  dssysolyatin/telegram2photoprism:latest dssysolyatin/telegram2photoprism:${VERSION}-arm64 dssysolyatin/telegram2photoprism:${VERSION}-amd64
 docker manifest push dssysolyatin/telegram2photoprism:latest
